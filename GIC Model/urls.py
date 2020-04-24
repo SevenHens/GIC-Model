a79a89ad88ce1ec1233f97d248ad95f2296fe5f5
SevenHens <56656803+SevenHens@users.noreply.github.com>
2020-04-24 03:28:25 -0400

    Add files via upload

--- GIC Model/urls.py
+++ GIC Model/urls.py
@@ -1,13 +1,23 @@
-from django.urls import path, include
-from . import views
-from rest_framework import routers
+"""djangoAPI URL Configuration
 
-router = routers.DefaultRouter()
-router.register('MyAPI', views.PredictView)
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from MyAPI import views
+
 urlpatterns = [
-    path('home/', views.home, name = 'home'),
-    path('Display/Renewal', views.display_predict_renewal, name = "file"),
-    path('Display/Purchase', views.display_predict_purchase, name = "file"),
-    path('Download/Renewal', views.download_predict_renewal, name = "file"),
-    path('Download/Purchase', views.download_predict_purchase, name = "file"),
+    path('admin/', admin.site.urls),
+    path('', include('MyAPI.urls')),
 ]
